058a9350972cf6a1e9f95c458e281764640c71d6
Sasha V <sancho11alex@gmail.com>
2017-11-02 21:25:50 +0200

    init

---
+++ Prognoz/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Prognoz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Legend = "Legend1";
+            series2.Name = "Series2";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(618, 249);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 267);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Загрузить и посчитать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(339, 270);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(291, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(642, 300);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Prognoz/Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prognoz
+{
+    public partial class Form1 : Form
+    {
+        private List<double> X;    // выборка с просмотрами
+        private List<double> Y;    // выборка с переходами
+        private List<double> X1;    // выборка со средними просмотрами
+        private List<double> Y1;    // выборка со средними переходами
+        private int N;                // кол-во элементов
+        private int N1;               // кол-во элементов в выборке средних
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                X = new List<double>();    // создаем экземпляр класса, выделяем память
+                Y = new List<double>();
+                X1 = new List<double>();
+                Y1 = new List<double>();
+                openFileDialog1.FileName = "";
+                openFileDialog1.ShowDialog();    // появление окна выбора файлов
+                string FileName = openFileDialog1.FileName;    // запоминание имени файлй
+                FileStream stream = File.Open(FileName, FileMode.Open, FileAccess.Read);     // файловый поток(не шарю)
+                if (stream != null)
+                {
+                    StreamReader reader = new StreamReader(stream, Encoding.Default);     // ридер файла(не шарю)
+                    for (int i = 0; !reader.EndOfStream; i++)      // цикл пока не достигнут конец файла
+                    {
+                        string str = reader.ReadLine();      // считывание строки с файла
+                        X1.Add(double.Parse(str));
+                    }
+                    stream.Close();    // закрытие потока
+                }
+                // далее тоже самое для 2-го файла
+                openFileDialog1.FileName = "";
+                openFileDialog1.ShowDialog();
+                FileName = openFileDialog1.FileName;
+                stream = File.Open(FileName, FileMode.Open, FileAccess.Read);
+                if (stream != null)
+                {
+                    StreamReader reader = new StreamReader(stream, Encoding.Default);
+                    for (int i = 0; !reader.EndOfStream; i++)
+                    {
+                        string str = reader.ReadLine();
+                        Y1.Add(double.Parse(str));
+                    }
+                    stream.Close();
+                }
+                N1 = X1.Count;    // сохранение кол-ва
+
+                KorrPole();     // построение корелляционного поля
+                srednekv();         // построение многочлена
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка. Файл не загружен!");
+            }
+        }
+
+        private void KorrPole()
+        {
+            foreach (var s in chart1.Series)
+                s.Points.Clear();                 // очистка серий
+            double sum = 0;
+            for (int i = 0; i < N1; i++)             // синхронная сортировка пузырьком по перой выборке с сохранением соответствий
+            {
+                for (int j = N1 - 1; j > i; j--)
+                {
+                    if (X1[j] < X1[j - 1])
+                    {
+                        double tmp1 = X1[j];
+                        X1[j] = X1[j - 1];
+                        X1[j - 1] = tmp1;
+                        double tmp2 = Y1[j];
+                        Y1[j] = Y1[j - 1];
+                        Y1[j - 1] = tmp2;
+                    }
+                }
+            }
+            for (int i = 0; i < 200; i++)           // вычисление средних в каждой группе
+            {
+                for (int j = 0; j < N1 / 200; j++)
+                {
+                    sum += X1[N1 / 200 * i + j];
+                }
+                sum /= (N1 / 200);
+                X.Add(sum);
+                sum = 0;
+            }
+            for (int i = 0; i < 200; i++)
+            {
+                for (int j = 0; j < N1 / 200; j++)
+                {
+                    sum += Y1[N1 / 200 * i + j];
+                }
+                sum /= (N1 / 200);
+                Y.Add(sum);
+                sum = 0;
+            }
+            N = X.Count;
+            for (int i = 0; i < N; i++)
+            {
+                chart1.Series[0].Points.AddXY(X[i], Y[i]);              // нанесение точек график
+            }
+        }
+
+        public static double[] Gauss(double[,] A, double[] B, int m)         // метод решения системы уравнений методом гаусса
+        {
+            int n = m - 1;
+            double h;
+            double[] Res = new double[m];
+            for (int i = 0; i <= n - 1; i++)
+            {
+                for (int j = i + 1; j <= n; j++)
+                {
+                    A[j, i] = -(A[j, i]) / (A[i, i]);
+                    for (int k = i + 1; k <= n; k++)
+                    {
+                        A[j, k] = A[j, k] + A[j, i] * A[i, k];
+                    }
+                    B[j] = B[j] + A[j, i] * B[i];
+                }
+            }
+            Res[n] = B[n] / A[n, n];
+            for (int i = n - 1; i >= 0; i--)
+            {
+                h = B[i];
+                for (int j = i + 1; j <= n; j++)
+                {
+                    h = h - Res[j] * A[i, j];
+                }
+                Res[i] = h / A[i, i];
+            }
+            return Res;
+        }
+        public void srednekv()                 // метод построения многочлена
+        {
+            int m = 10;
+            List<double> ListC = new List<double>();
+            for (int j = 0; j <= 2 * m; j++)               // вычисление коэффициентов в матрице Грамма
+            {
+                double sum = 0;
+                for (int i = 0; i < N; i++)
+                {
+                    sum += Math.Pow(X[i], j);
+                }
+                ListC.Add(sum);
+            }
+
+            List<double> ListD = new List<double>();       // вычисление свободных членов
+            for (int k = 0; k <= m; k++)
+            {
+                double sum = 0;
+                for (int i = 0; i < N; i++)
+                {
+                    sum += Math.Pow(X[i], k) * (Y[i]);
+                }
+                ListD.Add(sum);
+            }
+
+            double[,] Det = new double[m + 1, m + 1];         // формирование расширенной матрицы грамма
+            double[] Det0 = new double[m + 1];
+            for (int i = 0; i <= m; i++)
+            {
+                for (int j = 0; j <= m; j++)
+                {
+                    Det[i, j] = ListC[j + i];
+                }
+                Det0[i] = ListD[i];
+            }
+            double[] solution = Gauss(Det, Det0, m + 1);         // нахождение решения системы - коэффициентов
+            double res = 0;
+            chart1.Series[1].Color = Color.Black;
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j <= m; j++)
+                {
+                    res += solution[j] * Math.Pow(X[i], j);
+                }
+                chart1.Series[1].Points.AddXY(X[i], res);          // построение графика многочлена
+                res = 0;
+            }
+            string str = "";
+            foreach(var v in solution)
+            {
+                str += v.ToString() + ";";        // вывод коэффициентов с разделительным символом ; в текстовое поле для дальнейшего копирования
+            }
+            str = str.Remove(str.Length - 1, 1);      // удаление символа ; в конце
+            textBox1.Text = str;
+        }
+    }
+}
